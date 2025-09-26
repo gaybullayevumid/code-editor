dbdd5acabe5f214e2fd3c44c20e624f7a411b30f
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-09-26 22:14:49 +0500

    Remove unused files: delete CodeEditor, FileExplorer, GitManager, MainWindow, Terminal, and other related files

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 import sys
-from PyQt6.QtWidgets import QApplication
-from editor.main_window import MainWindow
+from PyQt6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    )
+from PyQt6.QtGui import QAction
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        self.setWindowTitle("PyIDLE")
+        self.setGeometry(100, 100, 900, 600)
+
+        menu_bar = self.menuBar()
+
+        file_menu = menu_bar.addMenu("File")
+        new_action = QAction("New Text File", self)
+        file_menu.addAction(new_action)
+
+        view_menu = menu_bar.addMenu("View")
+        full_screen_action = QAction("Fullscreen", self)
+        view_menu.addAction(full_screen_action)
 
 
-def main():
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec())
 
-
-if __name__ == "__main__":
-    main()
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+sys.exit(app.exec())
